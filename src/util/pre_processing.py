7c820c9dc74a4e3311a7260c1300703084b951f9
SunoopDogg <aswoo55555@gmail.com>
2024-10-05 23:26:07 +0900

    feat: Add text preprocessing functions

---
+++ src/util/pre_processing.py
@@ -0,0 +1,22 @@
+import re
+
+
+def remove_except_parentheses(text):
+    # 소괄호 내부를 제외하고 특수문자를 공백으로 치환
+    return re.sub(r'(?<!\()[^\w\s()](?![^\(]*\))', ' ', text)
+
+
+def get_token(text):
+    # 소괄호 내부를 제외하고 토큰화
+    protected_text = re.findall(r'\([^)]*\)', text)
+
+    for i, match in enumerate(protected_text):
+        text = text.replace(match, f'protected_text_{i}')
+
+    tokens = text.split()
+
+    for i, match in enumerate(protected_text):
+        tokens = [token.replace(
+            f'protected_text_{i}', match) for token in tokens]
+
+    return tokens
