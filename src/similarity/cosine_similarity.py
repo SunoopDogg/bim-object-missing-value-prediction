283862213110b7b7e5c52a46f362defe7fdd0f88
SunoopDogg <aswoo55555@gmail.com>
2024-10-26 04:31:03 +0900

    feat: Add TF-IDF based cosine similarity calculation in cosine_similarity.py

--- src/similarity/cosine_similarity.py
+++ src/similarity/cosine_similarity.py
@@ -2,8 +2,9 @@ import pandas as pd
 import numpy as np
 from numpy import dot
 from numpy.linalg import norm
+from sklearn.metrics.pairwise import cosine_similarity
 
-from util.pre_processing import get_token, remove_except_parentheses
+from util.pre_processing import get_tfidf_matrix, get_token, remove_except_parentheses
 
 
 def cos_sim(A, B):
@@ -39,6 +40,47 @@ def get_cosine_similarity(names, values):
                     A = make_matrix(set_tokens, value_tokens)
                     B = make_matrix(set_tokens, token)
                     similarity[token].append(cos_sim(A, B).item())
+
+        df = pd.DataFrame.from_dict(
+            similarity, orient='index', columns=values[idx])
+        df = df.transpose()
+
+        result.append(df)
+
+    return result
+
+
+def get_cosine_similarity_tf_idf(names, values):
+    result = []
+    size = len(names)
+    for idx, name in enumerate(names):
+        print(f"{idx+1}/{size}")
+        tokens = get_token(remove_except_parentheses(name))
+
+        similarity = {}
+        for token in tokens:
+            if similarity.get(token) is None:
+                similarity[token] = []
+            else:
+                continue
+
+            for value in values[idx]:
+                if pd.isna(value):
+                    similarity[token].append(0)
+                else:
+                    value_tokens = get_token(str(value))
+                    total_tokens = [token] + value_tokens
+                    # print(total_tokens)
+
+                    try:
+                        tfidf_matrix = get_tfidf_matrix(total_tokens)
+                    except:
+                        similarity[token].append(0)
+                        continue
+
+                    cosine_sim = cosine_similarity(tfidf_matrix, tfidf_matrix)
+
+                    similarity[token].append(cosine_sim[0][1])
 
         df = pd.DataFrame.from_dict(
             similarity, orient='index', columns=values[idx])
