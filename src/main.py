76ca1397708a46d60169c1b1fc5826720955a41b
SunoopDogg <aswoo55555@gmail.com>
2024-12-27 18:06:20 +0900

    refactor: Change main process structure

--- src/main.py
+++ src/main.py
@@ -1,102 +1,80 @@
-import json
-import os
-import pandas as pd
-from matplotlib import pyplot as plt
-from matplotlib import rc
+from util.pre_processing import combine_two_tokens, get_token, remove_except_parentheses
 
-from similarity.cosine_similarity import get_cosine_similarity, get_cosine_similarity_tf_idf
-# from similarity.gestalt_pattern_matching import get_gestalt_pattern_matching
-from similarity.gestalt_pattern_matching import get_gestalt_pattern_matching
-from similarity.jaccard_similarity import get_jaccard_similarity
-from similarity.sorensen_dice_coefficient import get_sorensen_dice_coefficient
-from util.change_extension import xlsx_to_json
-from util.pre_processing import get_tfidf_matrix, get_token, remove_except_parentheses
+from similarity.gestalt_pattern_matching import get_gestalt_pattern_matching_similarity
+from similarity.jaccard_similarity import get_jaccard_similarity_similarity
 
+from db.mongo_insert import insert_json_to_collection
+from db.mongo_select import find_all_documents
 
-def get_object_name(json_data):
-    object_name = []
-    for data in json_data:
-        object_name.append(data["Name"])
-    return object_name
+# 사용할 파일 이름 설정
+FILE_NAME = '속성테이블(프로세스)'
+
+DELIMITER_LIST = ['_', ' ', '-']
 
 
-def get_object_values(json_data):
-    object_values = []
-    for data in json_data:
-        values = []
-        for value in data.values():
-            if isinstance(value, dict):
-                values.extend(value.values())
+def get_similality_each_values(tokens, bim_object, func):
+    """
+    BIM 객체의 각 값에 대해 유사도를 계산하는 함수
+
+    Args:
+        tokens (list): 토큰 리스트
+        bim_object (dict): BIM 객체
+        func (function): 유사도 계산 함수
+
+    Returns:
+        dict: 유사도 결과
+    """
+    result = {}
+
+    for k, v in bim_object.items():
+        if isinstance(v, dict):
+            # 값이 딕셔너리인 경우 재귀 호출
+            result[k] = get_similality_each_values(tokens, v, func)
+        else:
+            similarity = {}
+
+            if v == '':
+                continue
             else:
-                values.append(value)
-        object_values.append(values)
-    return object_values
+                for token in tokens:
+                    similarity[token] = func(token, v)
+
+            result[k] = {str(v): similarity}
+
+    return result
 
 
 if __name__ == '__main__':
-    data_dir = os.path.join(os.getcwd(), "data")
-    json_dir = os.path.join(data_dir, "json")
+    # 'bim' 데이터베이스에서 모든 문서 조회
+    bim_objects = find_all_documents('bim', FILE_NAME)
 
-    file_name = "속성테이블(가양동)"
+    for bim_object in bim_objects[:1]:
+        # BIM 객체의 이름 가져오기
+        object_name = bim_object['Name']
 
-    # xlsx_to_json(file_name)
+        # 특정 키를 제외한 새로운 BIM 객체 생성
+        new_bim_object = {k: v for k, v in bim_object.items(
+        ) if k not in ['_id', 'Name', 'ObjectType', 'GlobalID', 'PredefinedType']}
 
-    json_path = os.path.join(json_dir, f"{file_name}.json")
-    json_data = None
+        # 객체 이름에서 토큰 추출
+        tokens = get_token(remove_except_parentheses(object_name))
+        # 토큰 조합하여 확장
+        extended_tokens = tokens.copy()
+        for delimiter in DELIMITER_LIST:
+            extended_tokens.extend(combine_two_tokens(tokens, delimiter))
 
-    with open(json_path, "r") as f:
-        json_data = json.load(f)
+        # 유사도 계산
+        # similarity_object = get_similality_each_values(
+        #     extended_tokens, new_bim_object, get_gestalt_pattern_matching_similarity)
+        similarity_object = get_similality_each_values(
+            extended_tokens, new_bim_object, get_jaccard_similarity_similarity)
 
-    names = get_object_name(json_data)
-    values = get_object_values(json_data)
+        # 결과 객체 생성
+        result_object = {}
+        result_object['Name'] = object_name
+        # result_object['Method'] = 'Gestalt Pattern Matching'
+        result_object['Method'] = 'Jaccard Similarity'
+        result_object.update(similarity_object)
 
-    result = get_cosine_similarity_tf_idf([names[1]], [values[1]])
-
-    # 각 result 컬럼의 최대값을 구함
-    max_values = result[0].max().values
-    max_token = result[0].idxmax().values
-    memo = set()
-    for i in range(len(max_token)):
-        temp = str(max_token[i])
-        if temp in memo:
-            max_token[i] = f"{max_token[i]}_{i}"
-        else:
-            max_token[i] = temp
-
-        memo.add(str(max_token[i]))
-
-    print(max_token)
-
-    # 각 result 컬럼의 최대값을 시각화
-    rc('font', family='AppleGothic')
-    plt.rcParams['axes.unicode_minus'] = False
-    plt.figure(figsize=(10, 11))
-    plt.title(f"{names[1]}")
-    plt.bar(max_token, max_values, width=0.5)
-
-    plt.xticks(rotation=90)
-    plt.subplots_adjust(bottom=0.4)
-
-    for i, v in enumerate(max_values):
-        # colum - value
-        plt.text(i, v, f"{result[0].columns[i]
-                          } - {v:.2f}", ha='center', va='bottom')
-
-    plt.show()
-
-# save_path = os.path.join(data_dir, "similarity", f"{
-#                          file_name}_cosine_tfidf.xlsx")
-
-# last_row = 0
-# size = len(result)
-# with pd.ExcelWriter(save_path) as writer:
-#     for idx, df in enumerate(result):
-#         print(f"{idx+1}/{size}")
-#         object_name = names[idx]
-#         object_name_df = pd.DataFrame(
-#             [object_name], columns=["Object Name"])
-#         object_name_df.to_excel(writer, startrow=last_row, index=False)
-#         last_row += len(object_name_df) + 1
-
-#         df.to_excel(writer, startrow=last_row, index=True)
-#         last_row += len(df) + 2
+        # 결과를 'bim_similarity' 컬렉션에 삽입
+        insert_json_to_collection('bim_similarity', FILE_NAME, [result_object])
