1592e6d73580e29a234b2a9ea275477c62491aed
SunoopDogg <aswoo55555@gmail.com>
2024-12-28 18:04:52 +0900

    feat: Add function to combine two tokens with a specified delimiter

--- src/util/pre_processing.py
+++ src/util/pre_processing.py
@@ -1,5 +1,7 @@
 import re
-from sklearn.feature_extraction.text import TfidfVectorizer
+from itertools import permutations
+
+# from sklearn.feature_extraction.text import TfidfVectorizer
 
 
 def remove_except_parentheses(text):
@@ -23,7 +25,23 @@ def get_token(text):
     return tokens
 
 
-def get_tfidf_matrix(corpus):
-    # TF-IDF 행렬 생성
-    vectorizer = TfidfVectorizer()
-    return vectorizer.fit_transform(corpus)
+def combine_two_tokens(tokens, delimiter):
+    """
+    토큰 리스트에서 두 개의 토큰을 주어진 구분자로 결합하는 함수
+
+    Args:
+        tokens (list): 토큰 리스트
+        delimiter (str): 토큰을 결합할 구분자
+
+    Returns:
+        list: 두 개의 토큰이 결합된 새로운 토큰 리스트
+    """
+    combined_tokens = []
+    for token1, token2 in permutations(tokens, 2):
+        combined_tokens.append(token1 + delimiter + token2)
+    return combined_tokens
+
+# def get_tfidf_matrix(corpus):
+#     # TF-IDF 행렬 생성
+#     vectorizer = TfidfVectorizer()
+#     return vectorizer.fit_transform(corpus)
