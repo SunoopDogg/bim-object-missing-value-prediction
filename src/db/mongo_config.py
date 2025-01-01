079d881525f2f5c67a71ff9f5eb462fb55c402fb
SunoopDogg <aswoo55555@gmail.com>
2025-01-01 18:01:59 +0900

    refactor: Refactor MongoConfig to remove db_name parameter and add db_config decorator

--- src/db/mongo_config.py
+++ src/db/mongo_config.py
@@ -2,11 +2,15 @@ import pymongo
 
 HOST = '172.17.0.3'
 PORT = 27017
-DB_NAME = 'bim'
 
 
 class MongoConfig:
-    def __init__(self, host=HOST, port=PORT, db_name=DB_NAME):
+    def __init__(self, host=HOST, port=PORT):
         self.client = pymongo.MongoClient(host, port)
-        self.db = self.client[db_name]
-        print(f"Connected to database: {db_name}")
+
+
+def db_config(func):
+    def wrapper(*args, **kwargs):
+        mongo_config = MongoConfig()
+        return func(mongo_config, *args, **kwargs)
+    return wrapper
